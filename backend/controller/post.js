01bf98908195b5b90d152014376203a3471d04f1
komalkharbanda <kkharbanda762gmail.com>
2023-02-18 19:42:05 +0530

    update in  api

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -66,3 +66,95 @@ exports.deletePost = async (req,res)=>{
   }
   
 }
+exports.likeAndUnlikePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.likes.includes(req.user._id)) {
+      const index = post.likes.indexOf(req.user._id);
+
+      post.likes.splice(index, 1);
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Post Unliked",
+      });
+    } else {
+      post.likes.push(req.user._id);
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Post Liked",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getPostOfFollowing = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const posts = await Post.find({
+      owner: {
+        $in: user.following,
+      },
+    }).populate("owner likes comments.user");
+
+    res.status(200).json({
+      success: true,
+      posts: posts.reverse(),
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+exports.updateCaption = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    post.caption = req.body.caption;
+    await post.save();
+    res.status(200).json({
+      success: true,
+      message: "Post updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const {createPost,deletePost} = require("../controller/post");
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePost,updateCaption} = require("../controller/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
-router.route("/post/delete/:id").delete(isAuthenticated,deletePost);
 
-
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeAndUnlikePost)
+   .put(isAuthenticated, updateCaption)
+  .delete(isAuthenticated, deletePost);
 
 
 
